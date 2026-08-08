15e1228a2f776c471d5cb065c85fea9120a7b6bf
ibrahim0908780850-code <ibrahim0908780850@gmail.com>
2026-08-08 11:22:49 +0200

    Update whatsapp.service.js

--- src/services/whatsapp.service.js
+++ src/services/whatsapp.service.js
@@ -1,40 +1,30 @@
 import axios from "axios";
 
+const GRAPH_VERSION = "v26.0";
 
-export async function sendWhatsAppMessage(
-    phone,
-    message
-){
+export async function sendWhatsAppMessage(phone, message) {
+  const url =
+    `https://graph.facebook.com/${GRAPH_VERSION}/` +
+    `${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`;
 
-
-const url =
-`https://graph.facebook.com/v25.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`;
-
-
-await axios.post(
-url,
-{
-    messaging_product:"whatsapp",
-
-    to:phone,
-
-    type:"text",
-
-    text:{
-        body:message
+  const response = await axios.post(
+    url,
+    {
+      messaging_product: "whatsapp",
+      to: phone,
+      type: "text",
+      text: {
+        body: message
+      }
+    },
+    {
+      headers: {
+        Authorization:
+          `Bearer ${process.env.WHATSAPP_ACCESS_TOKEN}`,
+        "Content-Type": "application/json"
+      }
     }
+  );
 
-},
-{
-headers:{
-    Authorization:
-    `Bearer ${process.env.WHATSAPP_ACCESS_TOKEN}`,
-
-    "Content-Type":
-    "application/json"
-}
-
-});
-
-
+  return response.data;
 }
